bc071b2546c07e4582b87bf6d07b23b4be6c7632
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-09-29 19:52:02 +0530

    Complete sign in page functionality

--- client/src/pages/SingIn.jsx
+++ client/src/pages/SingIn.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+
 
 function SingIn() {
+  const [formdata, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  //handle set input status
+  const handlechange = (e) => {
+    setFormData({
+      ...formdata,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  //handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formdata)
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+
+  console.log(formdata);
+
+
   return (
-    <div>SingIn</div>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Sing in</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="email" placeholder='email' className='p-3 border rounded-lg' id='email' onChange={handlechange} required />
+        <input type="password" placeholder='password' className='p-3 border rounded-lg' id='password' onChange={handlechange} required />
+        <button disabled={loading} className='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign in'}</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='mt-5 text-red-500' >{error}</p>}
+    </div>
   )
 }
 
--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -59,7 +59,7 @@ function SingUp() {
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to='sign-in'>
+        <Link to='/sign-in'>
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
