11c11470a36d9cc2d799f921d1ccc17953488852
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-09-29 13:43:15 +0530

    Complete sign up page Ul

--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function SingUp() {
   return (
-    <div>SingUp</div>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Sing Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='p-3 border rounded-lg' id='username' />
+        <input type="email" placeholder='email' className='p-3 border rounded-lg' id='email' />
+        <input type="password" placeholder='password' className='p-3 border rounded-lg' id='password' />
+        <button className='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='sign-in'>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
