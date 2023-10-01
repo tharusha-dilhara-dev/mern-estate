fdbfcec1d8419dff04a0f59cb087b4f9a85506d7
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-10-01 23:39:30 +0530

    complete update listing functionality

--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing ,deleteListing ,updateListing  } from '../controllers/listing.controller.js';
+import { createListing ,deleteListing ,updateListing,getListing   } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 
@@ -9,6 +9,7 @@ const router= express.Router();
 router.post('/create',verifyToken ,createListing );
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing';
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
         <Route path='/sign-in' element={<SingIn/>} />
         <Route path='/sign-up' element={<SingUp/>} />
         <Route path='/about' element={<About/>} />
+        <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         <Route  element={<PrivateRoute/>} >
           <Route path='/profile' element={<Profile/>} />
           <Route path='/create-listing' element={<CreateListing/>} />
