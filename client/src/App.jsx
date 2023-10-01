212bab1835b583ea53679e504df97b7dba39d2bf
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-10-02 00:30:18 +0530

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/sign-in' element={<SingIn/>} />
         <Route path='/sign-up' element={<SingUp/>} />
         <Route path='/about' element={<About/>} />
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
         <Route
             path='/update-listing/:listingId'
