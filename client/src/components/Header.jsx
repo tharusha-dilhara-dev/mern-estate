1c7af092b203a9088332176c9911ad9f9cf565cb
tharusha-dilhara-dev <td.jayadeera@gmail.com>
2023-10-02 00:28:57 +0530

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,56 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import {useSelector} from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react';
 
 function Header() {
-    const {currentUser} =useSelector(state=> state.user)
-  return (
-    <header className='shadow-md bg-slate-200'>
-        <div className='flex items-center justify-between max-w-6xl p-3 mx-auto'>
-            <Link to='/'>
-                <h1 className='flex-wrap text-sm font-bold sm:text-xl'>
-                    <span className='text-slate-500'>Milion</span>
-                    <span className='text-slate-500'>Estate</span>
-                </h1>
-            </Link>
-            <form className='flex items-center p-3 rounded-lg bg-slate-100' >
-                <input type="text" placeholder='Search..' className='w-24 bg-transparent focus:outline-none sm:w-64' />
-                <FaSearch className='text-slate-600' />
-            </form>
-            <ul className='flex gap-4'>
-                <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
-                <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-               
-                <Link to='/profile'>   {currentUser ? ( <img className='object-cover rounded-full h-7 w-7' src={currentUser.avatar} /> ) : (  <li className=' text-slate-700 hover:underline'>Sign in</li>    ) }    </Link>
-            </ul>
-        </div>
-    </header>
-  )
+    const { currentUser } = useSelector(state => state.user);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
+    return (
+        <header className='shadow-md bg-slate-200'>
+            <div className='flex items-center justify-between max-w-6xl p-3 mx-auto'>
+                <Link to='/'>
+                    <h1 className='flex-wrap text-sm font-bold sm:text-xl'>
+                        <span className='text-slate-500'>Milion</span>
+                        <span className='text-slate-500'>Estate</span>
+                    </h1>
+                </Link>
+                <form onSubmit={handleSubmit} className='flex items-center p-3 rounded-lg bg-slate-100'>
+                    <input type="text" placeholder='Search..' className='w-24 bg-transparent focus:outline-none sm:w-64' />
+                    <button>
+                        <FaSearch className='text-slate-600' />
+                    </button>
+                </form>
+                <ul className='flex gap-4'>
+                    <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
+                    <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
+
+                    <Link to='/profile'>   {currentUser ? (<img
+                        className='object-cover rounded-full h-7 w-7'
+                        src={currentUser.avatar}
+                        alt='profile'
+                    />) : (<li className=' text-slate-700 hover:underline'>Sign in</li>)}    </Link>
+                </ul>
+            </div>
+        </header>
+    )
 }
 
 export default Header
